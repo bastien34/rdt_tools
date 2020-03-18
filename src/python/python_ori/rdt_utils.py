1ce184748527e2fed9a03769744d4bc65a11d92a
Bastien Roques <bastien.roques1@gmail.com>
2020-03-18 13:07:58 +0100

    lower question - apply style

--- src/python/python_ori/rdt_utils.py
+++ src/python/python_ori/rdt_utils.py
@@ -13,8 +13,6 @@ from debug import mri
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger('doc_cleaner')
 
-QUESTION_CSS = "Inter Q"
-
 
 class Mission:
 
@@ -23,31 +21,15 @@ class Mission:
         self.smgr = self.ctx.ServiceManager
         self.desktop = self.smgr.createInstanceWithContext("com.sun.star.frame.Desktop", self.ctx )
         self.doc = self.desktop.getCurrentComponent()
-        self.text = self.doc.Text
         self._remove_blank_space_at_the_end_of_lines()
-        self._apply_question_style()
         logger.debug('Mission instanciated!')
 
     def _remove_blank_space_at_the_end_of_lines(self):
-        logger.debug('  - clean: remove blank space at end of lines')
         rd = self.doc.createReplaceDescriptor()
         rd.SearchRegularExpression = True
         rd.SearchString = '\s*$'
         rd.ReplaceString = ""
         self.doc.replaceAll(rd)
-
-    def _apply_question_style(self):
-        logger.debug('  - clean: apply style Inter Q')
-        text_enum = self.text.createEnumeration()
-
-        while text_enum.hasMoreElements():
-            element = text_enum.nextElement()
-            if element.supportsService("com.sun.star.text.Paragraph"):
-                # element.CharWeight is not reliable. We check first & last letter
-                # to determine if **element** is a question
-                if element.getStart().CharWeight == FontWeight.BOLD \
-                        or element.getEnd().CharWeight == FontWeight.BOLD:
-                    element.ParaStyleName = "Inter Q"
 
     def clean_timecode(self):
         logger.debug('Clean timecode')
@@ -76,74 +58,44 @@ class Mission:
         rd.ReplaceString = "[$1:$2:$3]"
         self.doc.replaceAll(rd)
 
-    def question_upper(self):
-        logger.debug('Upperise question')
-        text_enum = self.text.createEnumeration()
+    def upperise_question(self):
+        vtext = self.doc.Text
+        text_enum = vtext.createEnumeration()
 
         while text_enum.hasMoreElements():
             element = text_enum.nextElement()
             if element.supportsService("com.sun.star.text.Paragraph"):
-                if element.ParaStyleName == QUESTION_CSS:
+                # element.CharWeight is not reliable. We check first & last letter
+                # to determine if **element** is a question
+                if element.getStart().CharWeight == FontWeight.BOLD \
+                        or element.getEnd().CharWeight == FontWeight.BOLD:
+                    element.ParaStyleName = "Inter Q"
                     element.String = element.String.upper()
 
-    def question_lower(self):
-        logger.debug('question lower')
-        text_enum = self.text.createEnumeration()
-
-        while text_enum.hasMoreElements():
-            element = text_enum.nextElement()
-            if element.supportsService("com.sun.star.text.Paragraph"):
-                if element.ParaStyleName == QUESTION_CSS and element.String:
-                    q = f"{element.String[0:1].upper()}{element.String[1:].lower()}"
-                    element.String = q
-
     def remove_blank_line(self):
-        logger.debug('Remove blank lines')
         rd = self.doc.createReplaceDescriptor()
         rd.SearchRegularExpression = True
-        rd.SearchString = '^$'
+        rd.SearchString = '^\s*$'
         rd.ReplaceString = ""
         self.doc.replaceAll(rd)
 
-    def order_question(self):
-        logger.debug("order question")
-        text_enum = self.text.createEnumeration()
-
-        i = 0
-        while text_enum.hasMoreElements():
-            element = text_enum.nextElement()
-            if element.supportsService("com.sun.star.text.Paragraph"):
-                if element.ParaStyleName == QUESTION_CSS:
-                    element.String = f"{i} - {element.String}"
-                    i += 1
-
 
 def timecode_cleaner(*args):
-    logger.debug("call to timecode cleaner")
     doc = Mission()
     doc.clean_timecode()
 
 
 def order_question(*args):
-    logger.debug("call to order_question")
     doc = Mission()
-    doc.order_question()
+    doc.upperise_question()
 
 
-def question_upper(*args):
-    logger.debug("call to upperise question")
+def upperise_question(*args):
     doc = Mission()
-    doc.question_upper()
-
-
-def question_lower(*args):
-    logger.debug("call to lower question")
-    doc = Mission()
-    doc.question_lower()
+    doc.upperise_question()
 
 
 def remove_blank_line(*args):
-    logger.debug("call to remove_blank_lines")
     doc = Mission()
     doc.remove_blank_line()
 
@@ -151,7 +103,6 @@ def remove_blank_line(*args):
 g_exportedScripts = (
     timecode_cleaner,
     order_question,
-    question_upper,
-    question_lower,
+    upperise_question,
     remove_blank_line,
 )
