f80c1fc3ee7080936176b84065ec7a43ccd26c3a
Bastien Roques <bastien.roques1@gmail.com>
2020-09-14 12:00:36 +0200

    version 0.1.4 format timecode

--- src/oxt_gen.py
+++ src/oxt_gen.py
@@ -16,7 +16,7 @@ import xml.etree.ElementTree as ET
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger('post_gen_project')
-VERSION = "0.1.3"
+VERSION = "0.1.4"
 EXTENSION_NAME= "rdt_utils"
 OUTPUT = 'extension'
 
