80cb88d7e45f149598af9a8ef3127f4bdc5dd2f8
Bastien Roques <bastien.roques1@gmail.com>
2021-08-12 10:56:11 +0200

    new version

--- src/oxt_gen.py
+++ src/oxt_gen.py
@@ -15,7 +15,7 @@ import xml.etree.ElementTree as ET
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger('post_gen_project')
 
-VERSION = "0.3.8"
+VERSION = "0.3.9"
 EXTENSION_NAME= "rdt_utils"
 OUTPUT = 'extension'
 
